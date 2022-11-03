027ba33bd019f699994c39d5d5f9b6fd169cc90c
Micah Vanella <micah.vanella@gmail.com>
2022-11-03 16:14:09 -0400

    Initial project creation.

---
+++ OpenMathFlashCards.App/OpenMathFlashCards.App/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using OpenMathFlashCards.App.ViewModels;
+using OpenMathFlashCards.App.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace OpenMathFlashCards.App
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
---
+++ OpenMathFlashCards.App/OpenMathFlashCards.App/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using OpenMathFlashCards.App.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace OpenMathFlashCards.App.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
